d638fc1c1aa72593d8a48d14e99c5143e6376686
Richard Maxwell <richard@rsmaxwell.co.uk>
2021-10-23 19:33:18 +0100

    allow the dump dir to be specified with an envvar

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,8 @@
 
 set -x
 
-mkdir ~/mosquitto/config ~/mosquitto/data ~/mosquitto/log
+mkdir ~/mosquitto/config ~/mosquitto/log ~/mosquitto/data
+mkdir ~/players-tt-api/config ~/players-tt-api/log ~/players-tt-api/dump
 
 ls -al ~/mosquitto
+ls -al ~/players-tt-api
