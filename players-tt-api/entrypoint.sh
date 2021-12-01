84e1504947eb362152a7e764cd1466fe1bfc040d
Richard Maxwell <richard@rsmaxwell.co.uk>
2021-12-01 10:45:40 +0000

    re-engineer database initialisation

--- players-tt-api/entrypoint.sh
+++ players-tt-api/entrypoint.sh
@@ -8,7 +8,11 @@ export DEBUG_DEFAULT_FUNCTION_LEVEL="50"
 export DEBUG_PACKAGE_LEVEL_httphandler="50"
 export DEBUG_FUNCTION_LEVEL_httphandler_HandlerFunc="40"
 
-set -x
-ls -al /players-tt-api/config 1>/players-tt-api/log/listing.txt
 
-/home/fred/bin/players-tt-api --config /players-tt-api/config/config.json 1>/players-tt-api/log/players-tt-api.log 2>/players-tt-api/log/players-tt-api.stderr
+/home/fred/bin/players-tt-api-initialise --config /players-tt-api/config 1>/players-tt-api/log/players-tt-api.log 2>/players-tt-api/log/players-tt-api.stderr
+
+if [[ "${PLAYERS_TESTING}"="true" ]]; then
+    /home/fred/bin/players-tt-api-testdata --config /players-tt-api/config 1>/players-tt-api/log/players-tt-api.log 2>/players-tt-api/log/players-tt-api.stderr
+fi
+
+/home/fred/bin/players-tt-api --config /players-tt-api/config 1>/players-tt-api/log/players-tt-api.log 2>/players-tt-api/log/players-tt-api.stderr
